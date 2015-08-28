f43a9c3e601f95c6f4c5fe87f4e39a8a9b0ef5d9
Garrett <garrettweinert@gmail.com>
2015-08-28 12:48:16 -0700

    finished game- no 2 player

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var view = {
   buildGrid: function(){
 
     for(var i = 1; i <= 20*20; i++ ){
-      $('<div class="cell" id = "'+ i +'"></div>').appendTo('.game-grid')
+      $('<div class="cell" id = "'+ i +'"></div>').appendTo('.game-grid');
     }
 
   },
@@ -18,18 +18,20 @@ var view = {
     $('#190').addClass('head');
   },
 
+  updateScore: function(){
+    $('#score').html(model.score);
+  },
+
+
   keyListener: function() {
     $(document).keydown(function(e) {
-
+      
     switch(e.which) {
-        //if/elses disallow 180 degree turnarounds
+        //if/elses inside cases disallow 180 degree turnarounds if //snake has body
 
         //right
         case 37:
-          if (!controller.gameStart) {
-              controller.playGame();
-              controller.gameStart = true;
-            };
+          controller.hasGameStarted();
           if ( controller.vel[0] == 1 && controller.snakeDivs.length > 0){
             controller.vel = [1, 0];
           }
@@ -40,7 +42,7 @@ var view = {
 
         //down
         case 38:
-
+          controller.hasGameStarted();
           if ( controller.vel[1] == 1 && controller.snakeDivs.length > 0){
             controller.vel = [0, 1];
           }
@@ -51,6 +53,7 @@ var view = {
 
         //left
         case 39:
+          controller.hasGameStarted();
           if ( controller.vel[0] == -1 && controller.snakeDivs.length > 0){
             controller.vel = [-1, 0];
           }
@@ -61,6 +64,7 @@ var view = {
 
         //up
         case 40:
+          controller.hasGameStarted();
           if ( controller.vel[1] == -1 && controller.snakeDivs.length > 0){
             controller.vel = [0, -1];
           }
@@ -77,18 +81,22 @@ var view = {
 
   },
 
+
   buildSnakeFood: function(){
+    
+    //if no food on grid already create random grid coord
     if ($('.food').length === 0) {
       randomGridId = this.getRandomInt(1, 400);
+      
+      //creates new grid coord if food is placed on snake body
       while( randomGridId != controller.headPosition && controller.snakeDivs.indexOf(randomGridId) != -1){
         randomGridId = this.getRandomInt(1, 400);
       }
+      //adds food to game grid
       $('#' + randomGridId).addClass('food');
     }
 
   },
-
-  // isSnakeDiv
 
   getRandomInt: function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -102,28 +110,43 @@ var model = {
 
 var controller = {
 
+  //array of snake body parts
   snakeDivs: [],
-  updateInterval: 500,
+
+  //current interval speed
+  updateInterval: 400,
+
+  //var for interval functions in order to reset later
   snakeInterval: 1,
+
+  //sets gamestate to false until arrow key is pressed
   gameStart: false,
 
-  // snakeInterval: setInterval(function(){ controller.updatePosition(); }, this.updateInterval),
-  // snakeInterval: setInterval(function(){ this.updatePosition();}, controller.updateInterval),
-
+  //gets coord of div with class head
   headPosition: parseInt($('.head').attr("id")),
 
+  //sets direction of snake
+  vel: [0,0],
+
   init: function() {
+
     view.buildGrid();
     view.placeHead();
+
+    //starts browser listener for arrow keypresses
     view.keyListener();
   },
 
-  vel: [0,0],
+  hasGameStarted: function(){
+    if (!controller.gameStart) {
+      controller.playGame();
+      controller.gameStart = true;
+      console.log("startgin");
+    }
+  },
 
   playGame: function() {
     controller.snakeInterval = setInterval(function(){ controller.updatePosition();}, controller.updateInterval);
-
-    console.log(this.updateInterval);
 
   },
 
@@ -132,7 +155,6 @@ var controller = {
 
     switch(controller.vel.join()) {
       case "1,0":
-        console.log("MOVING RIGHT");
         return (nextHeadPosition % 20 === 1 || isNaN(nextHeadPosition));
         break;
 
@@ -140,27 +162,43 @@ var controller = {
         return (nextHeadPosition % 20 === 0 || isNaN(nextHeadPosition));
         break;
 
-      // case "0,1":
-      //   return isNaN(nextHeadPosition);
-      //   break;
-
-      // case "0,-1":
-      //   return isNaN(nextHeadPosition);
-      //   break;
-
       default:
         return isNaN(nextHeadPosition);
     }
 
   },
 
+  //if next head position is not included in snakebody array
+  // or if next head position is not a boundary 
+  //game is not over
   checkIfGameOver: function(nextHeadPosition) {
+
     if (controller.snakeDivs.indexOf(nextHeadPosition) === -1 && !controller.checkOutOfBounds(nextHeadPosition)) {
       return false;
     }
     else {
       return true;
     }
+  },
+
+  resetGame: function(){
+    
+    clearInterval(controller.snakeInterval);
+    controller.updateInterval = 400;
+
+    //sets gamestate to false so player can start with keypress
+    controller.gameStart = false;
+    
+    //removes all snake body parts from game grid and game
+    $('div').removeClass('snake-body');
+    controller.snakeDivs = [];
+
+    //resets snake head to middle
+    view.placeHead();
+    
+    //resets score to zero
+    model.score = 0;
+    view.updateScore();
   },
 
   updatePosition: function() {
@@ -175,18 +213,25 @@ var controller = {
     // Switch old head to snake body
     $('.head').addClass('snake-body').removeClass('head');
 
-    // Update head position
-    nextHeadPosition = controller.headPosition
+    // Update head position depending on last direction
+    nextHeadPosition = controller.headPosition;
     nextHeadPosition += controller.vel[0];
     nextHeadPosition += (controller.vel[1] * 20);
 
+    //checks if snake hits self or boundary
     if (controller.checkIfGameOver(nextHeadPosition)) {
-      clearInterval(controller.snakeInterval);
-      alert("Game Over");
+      lastScore = model.score;
+      
+      controller.resetGame();
+
+      alert("Game Over with score " + lastScore);
     }
     else {
+
+      //moves head to the next position
       $('#' + nextHeadPosition).addClass('head');
-      // Remove tail from snakeDivs
+
+      // Remove tail from snakeDivs to move snake forward
       tail = controller.snakeDivs.pop();
       $('#' + tail).removeClass('snake-body');
 
@@ -195,37 +240,44 @@ var controller = {
 
       // Spawn snake food
       view.buildSnakeFood();
-      console.log(nextHeadPosition);
+
     }
   },
 
+  resetInterval: function(){
+    
+    controller.updateInterval *= 0.95;
+
+    clearInterval(controller.snakeInterval);
+    
+    controller.snakeInterval = setInterval( controller.updatePosition, controller.updateInterval);
+  },
+
+
   eatSnakeFood: function() {
+
+      //if head position is at same spot as food position
     if (controller.headPosition === parseInt($('.food').attr("id"))) {
+
+      //updates snake body to be longer
       controller.snakeDivs.push($('food').attr('id'));
-      model.score += 1;
+
+      //removes food from game grid
       $('.food').removeClass('food');
-      this.updateInterval *= 0.85;
-      clearInterval(controller.snakeInterval);
-      controller.snakeInterval = setInterval( controller.updatePosition, controller.updateInterval);
+
+      //increases snake speed
+      controller.resetInterval();
+
+      //updates score in view
+      model.score += 1;
+      view.updateScore();
       }
+
     else {
-      return false;
+      // return false;
     }
-      // clearInterval(controller.snakeInterval);
-      // controller.snakeInterval = setInterval( controller.updatePosition, controller.updateInterval);
-      // console.log(snakeInterval);
-      // console.log(this.updateInterval);
+
   }
 
 };
 
-
-// Create head at center of grid
-// Game begins on keypress
-// Head moves in last direction of keypress every nth time
-// If cell is food
-//  Increase snake length
-// If cell is snake
-//  Game over
-// If cell is wall
-//  Game over
